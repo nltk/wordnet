c53db6590c97e8862f0b013193f7d89fe73d1191
alvations <alvations@gmail.com>
2019-04-24 16:54:58 +0800

    typos...

--- wn/lemma.py
+++ wn/lemma.py
@@ -54,7 +54,7 @@ class Lemma(WordNetObject):
     def count(self):
         if hasattr(self, '_count'):
             return self._count
-        if self._lang != 'eng' and self.key() in _lemmakey_to_count:
+        if self._lang != 'eng' or self.key() in _lemmakey_to_count:
             self._count = 0
         else:
             self._count = _lemmakey_to_count[self.key()]
