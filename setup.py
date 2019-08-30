b9447674ca02142b4f315f35dddb9507fcd19752
alvations <alvations@gmail.com>
2019-08-30 16:27:47 +0800

    added the wn 30 and 33 data

--- setup.py
+++ setup.py
@@ -10,13 +10,14 @@ from distutils.core import setup
 
 setup(
     name='wn',
-    version='0.0.21',
+    version='0.0.22',
     packages=['wn'],
     description='Wordnet',
     long_description='',
     url = 'https://github.com/alvations/wordnet',
     package_data={'wn': ['data/omw/*',
-                         'data/wordnet/*',
+                         'data/wordnet-3.0/*',
+                         'data/wordnet-3.3/*',
                          'data/wordnet_ic/*',]},
     license="MIT"
 )
