8c12ddf5393b22df1993dbb7c4e2524ade84da54
alvations <alvations@gmail.com>
2019-10-03 08:46:42 +0800

    Relicense

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
-#!/usr/bin/env python -*- coding: utf-8 -*-
+#!/usr/bin/env python
 #
-# Python Word Sense Disambiguation (pyWSD)
+# Setup script for Standalone WordNet API
 #
-# Copyright (C) 2014-17 alvations
-# URL:
-# For license information, see LICENSE.md
+# Copyright (C) 2019-2020 NLTK Project
+# Author:
+# URL: <http://nltk.org/>
+# For license information, see LICENSE.TXT
 
 from distutils.core import setup
 
 setup(
     name='wn',
-    version='0.0.20',
+    version='0.0.22',
     packages=['wn'],
     description='Wordnet',
     long_description='',
-    url = 'https://github.com/alvations/wordnet',
+    url = 'https://github.com/nltk/wordnet',
     package_data={'wn': ['data/omw/*',
-                         'data/wordnet/*',
+                         'data/wordnet-3.0/*',
+                         'data/wordnet-3.3/*',
                          'data/wordnet_ic/*',]},
-    license="MIT"
+    license="Apache License, Version 2.0"
 )
